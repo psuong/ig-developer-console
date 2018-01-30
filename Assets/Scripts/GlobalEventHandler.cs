54500d01248abcec33145b06a3c69243d71e844a
Porrith Suong <psuong95@gmail.com>
2018-01-30 00:42:22 -0500

    Refactors the global event table to use an Action<T>.

--- Assets/Scripts/GlobalEventHandler.cs
+++ Assets/Scripts/GlobalEventHandler.cs
@@ -2,35 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Stores global events to be freely accessed on a global level.
+/// </summary>
 public class GlobalEventHandler {
 
-	private static Dictionary<object, Dictionary<string, Delegate>> relativeEvents;
-	private static Dictionary<string, Delegate> globalEvents;
+	private static Dictionary<string, Action> globalEvents;
+	// TODO: Generify the object relative events
+	private static Dictionary<MonoBehaviour, Dictionary<string, Action>> monoRelativeEvents;
+	private static Dictionary<ScriptableObject, Dictionary<string, Action>> scriptableRelativeEvents;
 
-	private static void RegisterEvent (string eventName, Delegate handler) {
-		var d = default (Delegate);
-		if (GlobalEventHandler.globalEvents.TryGetValue (eventName, out d)) {
-			globalEvents[eventName] = Delegate.Combine (d, handler);
+	private static Action TryGetAction (string name) {
+		Action action;
+		globalEvents.TryGetValue (name, out action);
+		return action;
+	}
+
+	/// <summary>
+	/// Subscribes an event to the global table.
+	/// </summary>
+	/// <param name="name">The ID of the event</param>
+	/// <param name="action">The function to subscribe</param>
+	public static void SubscribeGlobalEvent (string name, Action action) {
+		if (GlobalEventHandler.globalEvents.ContainsKey (name)) {
+			globalEvents[name] += action;
 		} else {
-			globalEvents.Add (eventName, handler);
+			globalEvents.Add (name, action);
 		}
 	}
 
-	private static void RegisterEvent (object obj, string eventName, Delegate handler) {
-		if (obj == null) {
-			// Log an error here
-		} else {
-			var dictionary = default (Dictionary<string, Delegate>);
-			if (!GlobalEventHandler.relativeEvents.TryGetValue (obj, out dictionary)) {
-				dictionary = new Dictionary<string, Delegate> ();
-				relativeEvents.Add (obj, dictionary);
-			}
-			var d = default (Delegate);
-			if (dictionary.TryGetValue (eventName, out d)) {
-				dictionary[eventName] = Delegate.Combine (d, handler);
-			} else {
-				dictionary.Add (eventName, handler);
-			}
+	/// <summary>
+	/// Removes an event from the global table if it exists.
+	/// </summary>
+	/// <param name="name"></param>
+	/// <param name="action"></param>
+	public static void UnsubscribeGlobalEvent (string name, Action action) {
+		if (GlobalEventHandler.globalEvents.ContainsKey (name)) {
+			globalEvents[name] -= action;
+		}
+	}
+
+	/// <summary>
+	/// Invokes a global event given a valid event name.
+	/// </summary>
+	/// <param name="name">The ID of the event</param>
+	public static void InvokeEvent (string name) {
+		var action = TryGetAction (name);
+		if (action != null) {
+			action ();
 		}
 	}
 
