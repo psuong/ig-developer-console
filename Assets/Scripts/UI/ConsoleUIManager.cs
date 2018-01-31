9b6148400ce5b4adb8915e8b979734ed8729ec2e
Porrith Suong <psuong95@gmail.com>
2018-01-30 20:52:57 -0500

    Implements inputs to show editor console.

---
+++ Assets/Scripts/UI/ConsoleUIManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace DeveloperConsole {
+
+	[RequireComponent (typeof (CanvasGroup))]
+	public class ConsoleUIManager : MonoBehaviour {
+
+		[Header ("Canvas Properties")]
+		[SerializeField, Range (0f, 1f)]
+		private float startAlpha = 0f;
+		[SerializeField, Range (0f, 1f)]
+		private float endAlpha = 1f;
+		[SerializeField]
+		private KeyCode inputKey = KeyCode.BackQuote;
+
+		private CanvasGroup canvasGroup;
+		private bool isConsoleShowing;
+
+		private void Awake () {
+			isConsoleShowing = false;
+		}
+
+		private void Start () {
+			canvasGroup = GetComponent<CanvasGroup> ();
+			DisableConsole ();
+		}
+
+		private void Update () {
+			if (Input.GetKeyUp (KeyCode.BackQuote)) {
+				if (!isConsoleShowing) {
+					EnableConsole ();
+				} else {
+					DisableConsole ();
+				}
+			}
+		}
+
+		private void DisableConsole () {
+			canvasGroup.alpha = startAlpha;
+			canvasGroup.blocksRaycasts = canvasGroup.interactable = isConsoleShowing = false;
+		}
+
+		private void EnableConsole () {
+			canvasGroup.alpha = endAlpha;
+			canvasGroup.blocksRaycasts = canvasGroup.interactable = isConsoleShowing = true;
+		}
+
+	}
+}
