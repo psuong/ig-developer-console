113e3c7dfe76aaf8ec9efcd6f9eceeced9c7b7aa
Porrith Suong <psuong95@gmail.com>
2018-02-04 18:43:02 -0500

    Adds a scriptable object for the console history.

---
+++ Assets/Scripts/Console/ConsoleHistory.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DeveloperConsole {
+   
+    [CreateAssetMenu (fileName = "ConsoleHistory", menuName = "Developer Console/Console History")]
+    public class ConsoleHistory : ScriptableObject {
+
+        [SerializeField, Tooltip ("How many commands should we keep?")]
+        private int historySize = 20;
+
+        private int currentIndex;               // Stores the current index of what's added to the history
+        private IList<string> consoleHistory;   // Stores the console cmds
+
+        private void OnEnable () {
+            consoleHistory = new List<string> ();
+            currentIndex = 0;
+        }
+
+        public void AddConsoleCmd (string cmd) {
+            if (consoleHistory.Count < historySize) {
+                consoleHistory.Add (cmd);
+            } else {
+                // Treat the list like a stack-queue
+                consoleHistory.RemoveAt (0);
+                consoleHistory.Add (cmd);
+            }
+        }
+
+        public int IncrementHistory() {
+            return Mathf.Clamp (currentIndex++, 0, historySize);
+        }
+
+        public int DecrementHistory() {
+            return Mathf.Clamp (currentIndex--, 0, historySize);
+        }
+
+        public string GetRecentCommand () {
+            if (consoleHistory.Count > 0) {
+                return consoleHistory[currentIndex];
+            } else {
+                return string.Empty;
+            }
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/Editor/ConsoleHistoryInspector.cs
@@ -0,0 +1,58 @@
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEditor;
+
+namespace DeveloperConsole {
+
+    /// <summary>
+    /// This is used to see the ConsolesHistory within the inspector of the
+    /// scriptable object.
+    /// </summary>
+    [CustomEditor (typeof (ConsoleHistory))]
+    public class ConsoleHistoryInspector : Editor {
+
+        private const string ConsoleHistoryName = "consoleHistory";
+        private const string CurrentIndexName = "currentIndex";
+
+        private ConsoleHistory consoleHistory;
+        private System.Type type;
+        private FieldInfo consoleHistoryField;
+        private FieldInfo currentIndexField;
+
+        private void OnEnable () {
+            consoleHistory = target as ConsoleHistory;
+            type = consoleHistory.GetType ();
+
+            consoleHistoryField = type.GetField (ConsoleHistoryName, BindingFlags.NonPublic | BindingFlags.Instance);
+            currentIndexField = type.GetField (CurrentIndexName, BindingFlags.NonPublic | BindingFlags.Instance);
+        }
+
+        private void DrawConsoleHistory () {
+            EditorGUILayout.LabelField ("Console History", EditorStyles.boldLabel);
+            var list = (IList<string>) consoleHistoryField.GetValue (consoleHistory);
+
+            if (list != null) {
+                for (int i = 0; i < list.Count; i++) {
+                    var entry = list[i];
+                    EditorGUILayout.LabelField (string.Format ("Element {0}: {1}", i + 1, entry));
+                }
+            }
+        }
+
+        private void DrawCurrentIndex () {
+            EditorGUILayout.LabelField (string.Format ("Current Index: {0}", currentIndexField.GetValue(consoleHistory)));
+        }
+
+        public override bool RequiresConstantRepaint () {
+            return true;
+        }
+
+        public override void OnInspectorGUI () {
+            DrawDefaultInspector ();
+            DrawConsoleHistory ();
+            DrawCurrentIndex ();
+        }
+
+    }
+}
+
