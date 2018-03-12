2588071f74c827dab631013f20c8c6463294e9e0
Porrith Suong <psuong95@gmail.com>
2018-03-11 23:46:46 -0400

    Updates the GlobalEventHandler to support up to four arguments.

--- Assets/Scripts/Events/GlobalEventHandler.cs
+++ Assets/Scripts/Events/GlobalEventHandler.cs
@@ -40,7 +40,7 @@ namespace GlobalEvents {
         }
         
         /// <summary> 
-        /// Invokes a registered event within the global event table.
+        /// Invokes a function registered event within the global event table.
         /// </summary>
         /// <param name="eventName">The identifier for the event.</param>
         public static void InvokeEvent(string eventName) {
@@ -62,9 +62,37 @@ namespace GlobalEvents {
                 action(arg1, arg2);
             }
         }
-        
+
+        /// <summary> 
+        /// Invokes a registered event within the global event table using three arguments.
+        /// </summary>
+        /// <param name="eventName">The identifier for the event.</param>
+        /// <param name="arg1">The first argument to invoke the global event.</param>
+        /// <param name="arg2">The second argument to invoke the global event.</param>
+        /// <param name="arg3">The third argument to invoke the global event.</param>
+        public static void InvokeEvent<T1, T2, T3>(string eventName, T1 arg1, T2 arg2, T3 arg3) {
+            var action = GetDelegate(eventName, globalEventTable) as Action<T1, T2, T3>;
+            if (action != null) {
+                action(arg1, arg2, arg3);
+            }
+        }
+
+        /// <summary> 
+        /// Invokes a registered event within the global event table using four arguments.
+        /// </summary>
+        /// <param name="eventName">The identifier for the event.</param>
+        /// <param name="arg1">The first argument to invoke the global event.</param>
+        /// <param name="arg2">The second argument to invoke the global event.</param>
+        /// <param name="arg3">The third argument to invoke the global event.</param>
+        /// <param name="arg4">The fourth argument to invoke the glboal event.</param>
+        public static void InvokeEvent<T1, T2, T3, T4>(string eventName, T1 arg1, T2 arg2, T3 arg3, T4 arg4) {
+            var action = GetDelegate(eventName, globalEventTable) as Action<T1, T2, T3, T4>;
+            if (action != null) {
+                action(arg1, arg2, arg3, arg4);
+            }
+        }        
         /// <summary>
-        /// Is the event registered within the global event table?
+        /// Is there an event registered to the event table?
         /// </summary>
         /// <param name="eventName">The identifier of the event.</param>
         /// <returns>True, if registered.</returns>
@@ -73,7 +101,7 @@ namespace GlobalEvents {
         }
         
         /// <summary>
-        /// Registers an event to the global event table.
+        /// Registers a function to the global event table.
         /// </summary>
         /// <param name="eventName">The identifier for the event to register.</param>
         /// <param name="action">The function to register.</param>
@@ -82,7 +110,7 @@ namespace GlobalEvents {
         }
         
         /// <summary>
-        /// Registers an event to the global event table with two arguements.
+        /// Registers a function to the global event table with two arguements.
         /// </summary>
         /// <param name="eventName">The identifier for the event to register.</param>
         /// <param name="action">The function to register with two arguments.</param>
@@ -91,7 +119,23 @@ namespace GlobalEvents {
         }
         
         /// <summary>
-        /// Unregisters an event from the global event table.
+        /// Registers a function to the global event table with three arguments.
+        /// </summary>
+        /// <param name="eventName">The identifier for the event to register.</param>
+        /// <param name="action">The function to register with three arguments.</param>
+        public static void SubscribeEvent<T1, T2, T3>(String eventName, Action<T1, T2, T3> action) {
+            SubscribeEvent(eventName, action as Delegate);
+        }
+        
+        /// <summary>
+        /// Registers a function to the global event table with four arguments.
+        /// </summary>
+        public static void SubscribeEvent<T1, T2, T3>(String eventName, Action<T1, T2, T3, T4> action) {
+            SubscribeEvent(eventName, action as Delegate);
+        }
+
+        /// <summary>
+        /// Unregisters a function from the global event table.
         /// </summary>
         /// <param name="eventName">The identifier for the event to remove.</param>
         /// <param name="action">The function to remove.</param>
@@ -100,11 +144,29 @@ namespace GlobalEvents {
         }
         
         /// <summary>
-        /// Unregisters an event from the global event table.
+        /// Unregisters a function from the global event table.
         /// </summary>
         /// <param name="eventName">The identifier for the event to remove.</param>
         /// <param name="action">The function to remove.</param>
         public static void UnsubscribeEvent<T1, T2>(string eventName, Action<T1, T2> action) {
+            UnsubscribeEvent(eventName, action as Delegate);
+        }
+
+        /// <summary>
+        /// Unregisters a function from the global event table.
+        /// </summary>
+        /// <param name="eventName">The identifier for the event to remove.</param>
+        /// <param name="action">The function to remove.</param>
+        public static void UnsubscribeEvent<T1, T2, T3>(string eventName, Action<T1, T2, T3> action) {
+            UnsubscribeEvent(eventName, action as Delegate);
+        }
+
+        /// <summary>
+        /// Unregisters a function from the global event table.
+        /// </summary>
+        /// <param name="eventName">The identifier for the event to remove.</param>
+        /// <param name="action">The function to remove.</param>
+        public static void UnsubscribeEvent<T1, T2, T3, T4>(string eventName, Action<T1, T2, T3, T4> action) {
             UnsubscribeEvent(eventName, action as Delegate);
         }
     }
