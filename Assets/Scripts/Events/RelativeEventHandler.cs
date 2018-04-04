0f8b7c58afc09a3f85ffba1702c5b088b18a82ad
Porrith Suong <psuong95@gmail.com>
2018-04-04 07:11:14 -0400

    Reimplements the relative event handler with the use of reflections.

--- Assets/Scripts/Events/RelativeEventHandler.cs
+++ Assets/Scripts/Events/RelativeEventHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 
 namespace GlobalEvents {
 
@@ -7,5 +8,39 @@ namespace GlobalEvents {
 
     public static class RelativeEventHandler {
 
+        private static IDictionary<string, object> relativeEventTable = new Dictionary<string, object>();
+        
+        private static object GetRelativeObject(string methodName) {
+            var obj = default(object);
+            relativeEventTable.TryGetValue(methodName, out obj);
+            return obj;
+        }
+
+        private static void InvokeEvent(Type type, object instance, string methodName, params object[] args) {
+            try {
+                MethodInfo methodInfo = type.GetMethod(methodName, BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Public);
+                methodInfo.Invoke(instance, args);
+            } catch (Exception err) {
+#if UNITY_EDITOR
+                UnityEngine.Debug.LogError(err);
+#endif
+            }
+        }
+
+        private static void SubscribeEvent(string methodName, object instance) {
+            relativeEventTable.Add(methodName, instance);
+        }
+
+        private static void UnsubscribeEvent(string methodName) {
+            relativeEventTable.Remove(methodName);
+        }
+
+        public static void InvokeEvent(string methodName, params object[] args) {
+            var instance = GetRelativeObject(methodName);
+            if (instance != null) {
+                var type = instance.GetType();
+                InvokeEvent(type, instance, methodName, args);
+            }
+        }
     }
 }
