e8444792566e99396f9b9d18fd547baad825538f
Porrith Suong <psuong95@gmail.com>
2018-05-05 16:00:04 -0400

    Adds a walking kyle.

---
+++ Assets/Developer Console/Demo/Scripts/Patrol.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace Console.Demo {
+    
+    [RequireComponent(typeof(NavMeshAgent), typeof(AnimatorController))]
+    public class Patrol : MonoBehaviour {
+        
+        [SerializeField]
+        private Transform[] patrolPoints;
+        [SerializeField]
+        private float stoppingDistance = 1.5f;
+
+        private NavMeshAgent agent;
+        private AnimatorController controller;
+        private int index = -1;
+        
+        private void Start() {
+            agent = GetComponent<NavMeshAgent>();
+            controller = GetComponent<AnimatorController>();
+            
+            MoveToNextPoint();
+        }
+
+        private void Update() {
+            if (IsNearDestination()) {
+                MoveToNextPoint();
+            }
+        }
+
+        private void MoveToNextPoint() {
+            index = (index + 1) % patrolPoints.Length;
+            var position = patrolPoints[index].position;
+
+            agent.SetDestination(position);
+        }
+
+        private bool IsNearDestination() {
+            return Vector3.Distance(transform.position, agent.destination) <= stoppingDistance;
+        }
+    }
+}
