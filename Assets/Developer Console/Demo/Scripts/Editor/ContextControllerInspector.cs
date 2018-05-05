e95245bb6c5ffa9e9a9532e2782df1590d95bea1
Porrith Suong <psuong95@gmail.com>
2018-05-05 17:28:23 -0400

    Implements majority of the Inspector.

--- Assets/Developer Console/Demo/Scripts/Editor/ContextControllerInspector.cs
+++ Assets/Developer Console/Demo/Scripts/Editor/ContextControllerInspector.cs
@@ -1,10 +1,55 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Reflection;
+using UnityEditor;
 using UnityEngine;
 
 namespace Console.Demo.EditorTools {
 
-	public class ContextControllerInspector : MonoBehaviour {
+    [CustomEditor(typeof(ContextController))]
+    public class ContextControllerInspector : Editor {
 
-	}
+        private List<Patrol> activeAgents;
+        private List<Patrol> inactiveAgents;
+
+        private void OnEnable() {
+            var type = target.GetType();
+
+            var activeField = type.GetField("activeAIs", BindingFlags.NonPublic | BindingFlags.Instance);
+            activeAgents = activeField.GetValue(target) as List<Patrol>;
+
+            var inactiveField = type.GetField("inactiveAIs", BindingFlags.NonPublic | BindingFlags.Instance);
+            inactiveAgents = inactiveField.GetValue(target) as List<Patrol>;
+        }
+
+        private void DrawAgentTable() {
+            EditorGUILayout.LabelField("Patrol Agents", EditorStyles.boldLabel);
+
+            using (var group = new EditorGUILayout.VerticalScope()) {
+                EditorGUILayout.LabelField("Active", EditorStyles.boldLabel);
+                try {
+                    foreach(var agent in activeAgents) {
+                        EditorGUILayout.LabelField(agent.name);
+                    }
+                } catch(System.NullReferenceException) {}
+            }
+
+            using (var group = new EditorGUILayout.VerticalScope()) {
+                EditorGUILayout.LabelField("Active", EditorStyles.boldLabel);
+                try {
+                    foreach(var agent in inactiveAgents) {
+                        EditorGUILayout.LabelField(agent.name);
+                    }
+                } catch(System.NullReferenceException) {}
+            }
+        }
+
+        public override bool RequiresConstantRepaint() {
+            return EditorApplication.isPlaying;
+        }
+
+        public override void OnInspectorGUI() {
+            DrawDefaultInspector();
+        }
+
+    }
 }
