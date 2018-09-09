47fbe427ff76ec899709359fc22200d21507f216
Porrith Suong <psuong95@gmail.com>
2018-09-09 19:32:16 -0400

    Adds a test to subscribe, invoke, and unsubscribe all methods from the event handler.

--- Tests/Events/GlobalEventHandlerTests.cs
+++ Tests/Events/GlobalEventHandlerTests.cs
@@ -26,35 +26,71 @@ namespace Console.Tests {
         }
 
         [Test]
-        public void CheckingIfADelegateWasInvoked() {
-            var initialValue = 1;
-            var testHelper = new SubscriptionTestHelper(initialValue);
-            Assert.AreEqual(initialValue, testHelper.sum, "Value mismatch!");
+        public void SubscribingANoParamMethod() {
+            var initialValue = 0;
+            var result = GetResult(initialValue, 0);
+            Assert.AreEqual(1, result, "Value mismatch!");
+        }
 
-            // Subscribe the event
-            testHelper.Subscribe();
-
-            GlobalEventHandler.InvokeEvent(testHelper.AddEvent);
-
-            // Unsubscribe the event
-            testHelper.Unsubscribe();
-
-            Assert.AreEqual(initialValue + 1, testHelper.sum, "Value mismatch!");
+        [Test]
+        public void SubscribingAOneParamMethod() {
+            var initialValue = 2;
+            var result = GetResult(1, initialValue);
+            Assert.AreEqual(initialValue + 1, result, "Value mismatch!");
         }
 
         [Test]
         public void SubscribingATwoParamMethod() {
-            throw new NotImplementedException();
+            var initialValue = 1;
+            var result = GetResult(initialValue, 0, 2, 3);
+            Assert.AreEqual(5, result, "Value mismatch!");
         }
 
         [Test]
         public void SubscribeAThreeParamMethod() {
-            throw new NotImplementedException();
+            var initialValue = 0;
+            var result = GetResult(3, initialValue, 1, 2, 3);
+            Assert.AreEqual(6, result, "Value mismatch!");
         }
 
         [Test]
         public void SubscribeAFourParamMethod() {
-            throw new NotImplementedException();
+            var initialValue = 3;
+            var result = GetResult(4, initialValue, 2, 2, 2, 2);
+            Assert.AreEqual(11, result, "Value mismatch!");
+        }
+
+        private Tuple<int, SubscriptionTestHelper> CreateSubscriptionTestHelper(int value) {
+            return Tuple.Create(value, new SubscriptionTestHelper(value));
+        }
+
+        private int GetResult(int index, int initialValue, params int[] valuesToAdd) {
+            var tuple = CreateSubscriptionTestHelper(initialValue);
+            var testHelper = tuple.Item2;
+            Assert.AreEqual(tuple.Item1, tuple.Item2.sum, "Value mismatch!");
+
+            testHelper.Subscribe(index, valuesToAdd.Length);
+            var @event = testHelper[index];
+
+            switch(valuesToAdd.Length) {
+                case 4:
+                    GlobalEventHandler.InvokeEvent(@event, valuesToAdd[0], valuesToAdd[1], valuesToAdd[2], valuesToAdd[3]);
+                    break;
+                case 3:
+                    GlobalEventHandler.InvokeEvent(@event, valuesToAdd[0], valuesToAdd[1], valuesToAdd[2]);
+                    break;
+                case 2:
+                    GlobalEventHandler.InvokeEvent(@event, valuesToAdd[0], valuesToAdd[1]);
+                    break;
+                case 1:
+                    GlobalEventHandler.InvokeEvent(@event, valuesToAdd[0]);
+                    break;
+                default:
+                    GlobalEventHandler.InvokeEvent(testHelper[index]);
+                    break;
+            }
+
+            return testHelper.sum;
         }
     }
 }
--- Tests/Events/SubscriptionTestHelper.cs
+++ Tests/Events/SubscriptionTestHelper.cs
@@ -2,33 +2,78 @@
 
     public class SubscriptionTestHelper {
 
-        public readonly string AddEvent = "add";
         public int sum;
+        public readonly string[] AddEvents;
+
+        /// <summary>
+        /// Returns the string within the AddEvents array.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public string this[int key] {
+            get { return AddEvents[key]; }
+        }
 
         /// <summary>
         /// A basic constructor to allow for configurable testing with delegates.
         /// </summary>
         /// <param name="sum">The initial value of the sum.</param>
         public SubscriptionTestHelper(int sum) {
+            AddEvents = new[] { "add", "add1", "add2", "add3", "add4" };
             this.sum = sum;
         }
 
         /// <summary>
-        /// Use this to subscribe the AddOne method to the global event table.
+        /// Use this to subscribe the Add method to the global event table.
         /// </summary>
-        public void Subscribe() {
-            GlobalEventHandler.SubscribeEvent(AddEvent, AddOne);
+        /// <param name="i">The key of the AddEvents array.</param>
+        public void Subscribe(int i, int size) {
+            var @event = AddEvents[i];
+            switch (size) {
+                case 4:
+                    GlobalEventHandler.SubscribeEvent<int, int, int, int>(@event, Add);
+                    return;
+                case 3:
+                    GlobalEventHandler.SubscribeEvent<int, int, int>(@event, Add);
+                    return;
+                case 2:
+                    GlobalEventHandler.SubscribeEvent<int, int>(@event, Add);
+                    return;
+                case 1:
+                    GlobalEventHandler.SubscribeEvent<int>(@event, Add);
+                    return;
+                default:
+                    GlobalEventHandler.SubscribeEvent(@event, Add);
+                    return;
+            }
         }
 
         /// <summary>
-        /// Use this to remove the AddOne method to the global event table.
+        /// Use this to remove the Add method to the global event table.
         /// </summary>
-        public void Unsubscribe() {
-            GlobalEventHandler.UnsubscribeEvent(AddEvent, AddOne);
+        /// <param name="i">The key of the AddEvents array.</param>
+        public void UnsubscribeOne(int i) {
+            GlobalEventHandler.UnsubscribeEvent(AddEvents[i], Add);
         }
 
-        private void AddOne() {
+        private void Add() {
             sum++;
+        }
+
+        private void Add(int a) {
+            sum += a;
+        }
+
+        private void Add(int a, int b) {
+            sum += (a + b);
+        }
+
+        private void Add(int a, int b, int c) {
+            sum += (a + b + c);
+        }
+
+        private void Add(int a, int b, int c, int d) {
+            sum += (a + b + c + d);
         }
     }
 }
